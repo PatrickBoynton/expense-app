cebbb62d32f8dc33326090e9831a8de6f7fba479
PatrickBoynton <jpboynton3@gmail.com>
2021-05-26 16:48:04 -0400

    Removed the hard coded data and used props instead.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem({expenses}) {
+
+
     return (
         <div className="expense-item">
-            <time>March 28, 2021</time>
+            <time>{expenses.date.toDateString()}</time>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <span className="expense-item__price">$249.67</span>
+                <h2>{expenses.title}</h2>
+                <span className="expense-item__price">${expenses.amount}</span>
             </div>
         </div>
     );
