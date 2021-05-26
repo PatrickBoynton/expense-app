422bca3adf0a7fcef58c3bdc155986e2712cbf65
PatrickBoynton <jpboynton3@gmail.com>
2021-05-26 16:47:36 -0400

    Added the starting data, and a couple of ExpenseItems.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+    const expenses = [
+        {title: "Car Insurance", amount: 294.67, date: new Date(2021, 5, 26)},
+        {title: "New Shoes", amount: 100.99, date: new Date(2021, 3, 22)},
+        {title: "New Monitor", amount: 400.99, date: new Date(2021, 1, 12)}
+    ];
+
     return <>
         <h1>Let's get Started</h1>
-        <ExpenseItem>
-        </ExpenseItem>
+        <ExpenseItem expenses={expenses[0]}/>
+        <ExpenseItem expenses={expenses[1]}/>
+        <ExpenseItem expenses={expenses[2]}/>
     </>;
 }
 
