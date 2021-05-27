4c7e7061cbbb64c0f1405787d0d2ad03fb93b721
PatrickBoynton <jpboynton3@gmail.com>
2021-05-27 04:07:11 -0400

    Changed the div to the card.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem({expenses}) {
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate expenses={expenses}/>
             <div className="expense-item__description">
                 <h2>{expenses.title}</h2>
                 <span className="expense-item__price">${expenses.amount}</span>
             </div>
-        </div>
+        </Card>
     );
 }
 
