74f87ab8781be9e51f4319e526fd6335e5f5fc3b
PatrickBoynton <jpboynton3@gmail.com>
2021-05-27 18:57:54 -0400

    Moved the card into it's own folder.

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,11 @@
 import React from "react";
-import './Card.css';
-function Card({children, className}) {
+import '../Card.css';
+
+const Card = ({children, className}) => {
     const classes = 'card ' + className;
     return (
         <div className={classes}>{children}</div>
     );
-}
+};
 
 export default Card;
