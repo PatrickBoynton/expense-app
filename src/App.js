337c4dae74863d720bdc385ee1ffc0dde1a7ac92
PatrickBoynton <jpboynton3@gmail.com>
2021-05-30 16:16:47 -0400

    Added the new expense component.. Also did some formatting.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/ExpenseInput/NewExpense';
 
 function App() {
-  const expenses = [
-    { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 5, 26) },
-    { title: 'New Shoes', amount: 100.99, date: new Date(2021, 3, 22) },
-    { title: 'New Monitor', amount: 400.99, date: new Date(2021, 1, 12) },
-  ];
+    const expenses = [
+        { title: 'Car Insurance', amount: 294.67, date: new Date(2021, 5, 26) },
+        { title: 'New Shoes', amount: 100.99, date: new Date(2021, 3, 22) },
+        { title: 'New Monitor', amount: 400.99, date: new Date(2021, 1, 12) },
+    ];
 
-  return (
-    <>
-      <h1>Let's get Started</h1>
-      <Expenses expenses={expenses} />
-    </>
-  );
+    return (
+        <>
+            <NewExpense/>
+            <Expenses expenses={ expenses }/>
+        </>
+    );
 }
 
 export default App;
