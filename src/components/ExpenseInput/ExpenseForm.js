fa798ee5dc15aebcf79183f08700ee995a30627f
PatrickBoynton <jpboynton3@gmail.com>
2021-05-31 00:59:42 -0400

    Added input handler, state, and submit handler.

--- src/components/ExpenseInput/ExpenseForm.js
+++ src/components/ExpenseInput/ExpenseForm.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const [inputValue, setInputValue] = useState({title: '', amount: 0, date: new Date()});
+
+    const handleInput = (event) => {
+        setInputValue({
+            ...inputValue,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(inputValue);
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label htmlFor="title">Title</label>
-                    <input type="text" name="title" id="title"/>
+                    <input type="text"
+                           value={inputValue.title}
+                           name="title"
+                           id="title"
+                           onChange={ handleInput }/>
                 </div>
                 <div className="new-expense__control">
                     <label htmlFor="amount">Amount</label>
-                    <input type="number" name="amount" id="amount" min="0.00" step="0.01"/>
+                    <input type="number"
+                           name="amount"
+                           id="amount"
+                           value={inputValue.amount}
+                           onChange={ handleInput }
+                           min="0.00"
+                           step="0.01"/>
                 </div>
                 <div className="new-expense__control">
                     <label htmlFor="date">Date</label>
-                    <input type="date" name="date" id="date" min="2021-01-01"/>
+                    <input type="date"
+                           name="date"
+                           id="date"
+                           value={inputValue.date}
+                           onChange={handleInput}
+                           min="2021-01-01"/>
                 </div>
             </div>
             <div className="new-expense__actions">
