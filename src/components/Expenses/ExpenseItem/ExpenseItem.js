0fd5913c25eb4113db2a7375041d731879f6037c
PatrickBoynton <jpboynton3@gmail.com>
2021-05-31 13:17:56 -0400

    Added statae.

--- src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import Card from '../../UI/Card';
 
-const ExpenseItem = ({ expenses }) => (
-  <Card className="expense-item">
-    <ExpenseDate expenses={expenses} />
-    <div className="expense-item__description">
-      <h2>{expenses.title}</h2>
-      <span className="expense-item__price">${expenses.amount}</span>
-    </div>
-  </Card>
-);
+const ExpenseItem = ({ expenses }) => {
+  const [title, setTitle] = useState(expenses.title);
+
+  const handleClick = () => {
+    setTitle('This was fun!');
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate expenses={expenses} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <span className="expense-item__price">${expenses.amount}</span>
+      </div>
+      <button onClick={handleClick}>Change Title</button>
+    </Card>
+  );
+};
 
 export default ExpenseItem;
