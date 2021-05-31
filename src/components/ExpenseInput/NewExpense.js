b3fd444c07676058cfb74f54fc487843e0f46db7
PatrickBoynton <jpboynton3@gmail.com>
2021-05-31 17:52:40 -0400

    Created a method to handle creating a new expense.

--- src/components/ExpenseInput/NewExpense.js
+++ src/components/ExpenseInput/NewExpense.js
@@ -3,9 +3,17 @@ import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = () => {
+    const handleSubmitExpense = (data) => {
+        const expenseData = {
+            ...data,
+            // Todo: Remove this when you hook up the API.
+            id: Math.round(Math.random() * 10000)
+        }
+        console.log(expenseData);
+    }
     return (
         <div className="new-expense">
-            <ExpenseForm/>
+            <ExpenseForm onSubmitExpense={handleSubmitExpense} />
         </div>
     );
 };
