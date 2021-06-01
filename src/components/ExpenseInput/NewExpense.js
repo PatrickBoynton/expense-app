3a005a33017861c9244585c811f5f054fc9169cc
PatrickBoynton <jpboynton3@gmail.com>
2021-05-31 22:35:40 -0400

    Changed the log to a method.

--- src/components/ExpenseInput/NewExpense.js
+++ src/components/ExpenseInput/NewExpense.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
-const NewExpense = () => {
+const NewExpense = ({ onAddExpense }) => {
     const handleSubmitExpense = (data) => {
         const expenseData = {
             ...data,
             // Todo: Remove this when you hook up the API.
             id: Math.round(Math.random() * 10000)
-        }
-        console.log(expenseData);
-    }
+        };
+        onAddExpense(expenseData);
+    };
     return (
         <div className="new-expense">
-            <ExpenseForm onSubmitExpense={handleSubmitExpense} />
+            <ExpenseForm onSubmitExpense={ handleSubmitExpense }/>
         </div>
     );
 };
