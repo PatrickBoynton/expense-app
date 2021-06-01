a1c27a9f3c7db935d18ca7b69f4c3bb00e3f7a02
PatrickBoynton <jpboynton3@gmail.com>
2021-05-31 22:35:16 -0400

    Refactoring. Also added a state for the filter.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem/ExpenseItem';
 import Card from '../UI/Card';
+import ExpenseFilter from './ExpenseFilter/ExpenseFilter';
 
-const Expenses = ({ expenses }) => (
-  <Card>
-    <ExpenseItem expenses={expenses[0]} />
-    <ExpenseItem expenses={expenses[1]} />
-    <ExpenseItem expenses={expenses[2]} />
-  </Card>
-);
+const Expenses = ({ expenses }) => {
+    const [year, setYear] = useState('');
+
+    const changeYear = (event) => {
+        setYear(event.target.value);
+    };
+    return (
+        <>
+            <ExpenseFilter onChangeYear={ changeYear }/>
+            <Card>
+                { expenses.map(expense => <ExpenseItem key={expense.id}
+                                                       year={ year }
+                                                       expenses={ expense }/>) }
+            </Card>
+        </>
+    );
+};
 
 export default Expenses;
