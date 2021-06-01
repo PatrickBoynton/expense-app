09409c6f031ff0dfe1d29f48943b1f8995109e0f
PatrickBoynton <jpboynton3@gmail.com>
2021-05-31 22:34:29 -0400

    Refactoring.

--- src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -1,24 +1,18 @@
-import React, { useState } from 'react';
+import React  from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 import Card from '../../UI/Card';
 
 const ExpenseItem = ({ expenses }) => {
-  const [title, setTitle] = useState(expenses.title);
-
-  const handleClick = () => {
-    setTitle('This was fun!');
-  };
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate expenses={expenses} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <span className="expense-item__price">${expenses.amount}</span>
-      </div>
-      <button onClick={handleClick}>Change Title</button>
-    </Card>
+          <Card className="expense-item">
+              <ExpenseDate expenses={expenses} />
+              <div className="expense-item__description">
+                  <h2>{expenses.title}</h2>
+                  <span className="expense-item__price">${expenses.amount}</span>
+              </div>
+          </Card>
   );
 };
 
