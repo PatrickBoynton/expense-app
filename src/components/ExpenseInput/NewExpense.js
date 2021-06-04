9b3f46a23eca5a8e9d429696f189535605ede456
PatrickBoynton <jpboynton3@gmail.com>
2021-06-04 17:43:58 -0400

    Changed the way expenseData was added.

--- src/components/ExpenseInput/NewExpense.js
+++ src/components/ExpenseInput/NewExpense.js
@@ -5,9 +5,11 @@ import ExpenseForm from './ExpenseForm';
 const NewExpense = ({ onAddExpense }) => {
     const handleSubmitExpense = (data) => {
         const expenseData = {
-            ...data,
             // Todo: Remove this when you hook up the API.
-            id: Math.round(Math.random() * 10000)
+            id: Math.round(Math.random() * 10000),
+            title: data.title,
+            amount: data.amount,
+            date: new Date(data.date),
         };
         onAddExpense(expenseData);
     };
