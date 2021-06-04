b64ffabf10f33515e3627b3a85e673eeb6110049
PatrickBoynton <jpboynton3@gmail.com>
2021-06-04 17:42:54 -0400

    Added the set state for adding an expense.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/ExpenseInput/NewExpense';
+import { useState } from 'react';
+
+const initialState = [
+    { id: 1, title: 'Car Insurance', amount: 294.67, date: new Date(2021, 5, 26) },
+    { id: 2, title: 'New Shoes', amount: 100.99, date: new Date(2021, 3, 22) },
+    { id: 3, title: 'New Monitor', amount: 400.99, date: new Date(2021, 1, 12) },
+];
 
 function App() {
-    const expenses = [
-        { id: 1, title: 'Car Insurance', amount: 294.67, date: new Date(2021, 5, 26) },
-        { id: 2, title: 'New Shoes', amount: 100.99, date: new Date(2021, 3, 22) },
-        { id: 3, title: 'New Monitor', amount: 400.99, date: new Date(2021, 1, 12) },
-    ];
+    const [expenses, setExpenses] = useState(initialState);
+
 
     const addExpense = expense => {
-        console.log('In App Js.');
-        console.log(expense);
+        setExpenses([expense, ...expenses]);
     };
 
     return (
