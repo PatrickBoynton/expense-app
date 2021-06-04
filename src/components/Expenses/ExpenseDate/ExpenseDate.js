b6ac751989d3069fdc60cff59143cdd4c991147e
PatrickBoynton <jpboynton3@gmail.com>
2021-06-04 17:43:16 -0400

    Added a space.

--- src/components/Expenses/ExpenseDate/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate/ExpenseDate.js
@@ -5,6 +5,7 @@ const ExpenseDate = ({ expenses }) => {
   const month = expenses.date.toLocaleString('en-US', { month: 'long' });
   const day = expenses.date.toLocaleString('en-US', { day: '2-digit' });
   const year = expenses.date.getFullYear();
+
   return (
     <time className="expense-date">
       <div className="expense-date__month">{month}</div>
