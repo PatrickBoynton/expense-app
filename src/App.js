ab67b5a369014d1a54ee8615b80b42cf1a47cf8d
PatrickBoynton <jpboynton3@gmail.com>
2021-06-04 22:27:51 -0400

    Changed the dates.

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import NewExpense from './components/ExpenseInput/NewExpense';
 import { useState } from 'react';
 
 const initialState = [
-    { id: 1, title: 'Car Insurance', amount: 294.67, date: new Date(2021, 5, 26) },
+    { id: 1, title: 'Car Insurance', amount: 294.67, date: new Date(2022, 5, 26) },
     { id: 2, title: 'New Shoes', amount: 100.99, date: new Date(2021, 3, 22) },
-    { id: 3, title: 'New Monitor', amount: 400.99, date: new Date(2021, 1, 12) },
+    { id: 3, title: 'New Monitor', amount: 400.99, date: new Date(2019, 1, 12) },
 ];
 
 function App() {
