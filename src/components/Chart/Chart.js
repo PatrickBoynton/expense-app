d0b908bde330e5399ab09cba66c005ee96d2b896
PatrickBoynton <jpboynton3@gmail.com>
2021-06-07 00:48:47 -0400

    Added logic to get the total value.

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ChartBar from './ChartBar/ChartBar';
 import './Chart.css';
 
-const Chart = ({data, value, label}) => {
+const Chart = ({data}) => {
+    const dataPoints =  data.map(point => point.value);
+    const maxValue = Math.max(...dataPoints);
+
     return (
-        <section className="chart">
-            {data.map(point => <ChartBar key={label}
-                                         value={value}
-                                         maxValue={null}
-                                         label={label}/> )}
+        <section  className="chart">
+            {data.map(point => <ChartBar key={point.label}
+                                         value={point.value}
+                                         maxValue={maxValue}
+                                         label={point.label}/> )}
         </section>
     );
 };
